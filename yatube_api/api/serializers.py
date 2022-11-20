33077fafe5b1493e5d09bafe5bb3235b7df45dca
rtxOG <cemenchern001@gmail.com>
2022-11-20 21:25:36 +0300

    Fix: Base64 coding for images

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from django.contrib.auth import get_user_model
+from django.core.files.base import ContentFile
 from rest_framework.validators import UniqueTogetherValidator
+import base64
 
 from posts.models import Comment, Post, Group, Follow
 
 User = get_user_model()
+
+
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+        return super().to_internal_value(data)
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -14,6 +25,7 @@ class PostSerializer(serializers.ModelSerializer):
         queryset=Group.objects.all()
     )
     author = SlugRelatedField(slug_field='username', read_only=True)
+    image = Base64ImageField(required=False, allow_null=True)
 
     class Meta:
         fields = '__all__'
